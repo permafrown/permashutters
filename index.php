0da12ac0c5d675653f163138897b8d51f2f85eda
Joel Harsevoort <bjoelh@gmail.com>
2017-05-24 11:43:07 -0400

    adding other pages
    also switching to php

---
+++ index.php
@@ -0,0 +1,82 @@
+<!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css?family=Bevan|Cinzel|EB+Garamond|Lato|Libre+Baskerville|Overlock|Poiret+One|Righteous|Roboto+Condensed|Source+Sans+Pro|Tangerine|Montserrat" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/animate-master/animate.css">
+  <link rel="stylesheet" href="css/style.css">
+  <title>permashutters</title>
+  <div class="container-fluid top_nav">
+    <div class="row">
+      <div class="col-xs-12">
+        <ul class="topnav" id="top_nav_burg">
+          <li><a href="#family">family</a></li>
+          <li><a href="#sundry">sundry</a></li>
+          <li><a href="#words">words</a></li>
+          <li><a href="#science">science</a></li>
+          <li><a href="#fauna">fauna</a></li>
+          <li><a href="#games">games</a></li>
+          <li><a href="#home">home</a></li>
+          <li class="icon">
+            <a href="javascript:void(0);" onclick="hamburg()">&#9776;</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</head>
+<body>
+  <h1 class="title_centered" style="margin-top: 10%;">permashutters on git</h1>
+  <h2 class="animated pulse">games | fauna | science | words | sundry | family</h2>
+  <div class="container-fluid">
+    <div class="col-xs-12">
+      <img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" />
+    </div>
+  </div>
+    <div class="column shutters">
+      <div class="col-xs-2 shutt" id="games_shutter">
+        <a href="#games"><div class="vert_text">games</div></a>
+      </div>
+      <div class="col-xs-2 shutt" id="fauna_shutter">
+        <a href="#fauna"><div class="vert_text">fauna</div></a>
+      </div>
+      <div class="col-xs-2 shutt" id="science_shutter">
+        <a href="#science"><div class="vert_text">science</div></a>
+      </div>
+      <div class="col-xs-2 shutt" id="words_shutter">
+        <a href="#words"><div class="vert_text">words</div></a>
+      </div>
+      <div class="col-xs-2 shutt" id="sundry_shutter">
+        <a href="#sundry"><div class="vert_text">sundry</div></a>
+      </div>
+      <div class="col-xs-2 shutt" id="family_shutter">
+        <a href="#family"><div class="vert_text">family</div></a>
+      </div>
+    </div>
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/npm.js"></script>
+  <script src="js/permafrown.js"></script>
+</body>
+
+<footer>
+  <div class="container bot_nav">
+    <div class="row">
+      <div class="col-xs-12 foot_nav">
+        <button class="btn"><a href="https://www.facebook.com/">fb</a></button>
+        <button class="btn"><a href="https://twitter.com/">tw</a></button>
+        <button class="btn"><a href="https://www.chess.com/member/permafrown">chs</a></button>
+        <button class="btn"><a href="https://www.freecodecamp.com/permafrown">fcc</a></button>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</html>

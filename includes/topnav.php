a6be643e8ab9d01f497f1f4503628fc2bc738396
Joel Harsevoort <bjoelh@gmail.com>
2017-05-24 16:07:04 -0400

    go

---
+++ includes/topnav.php
@@ -0,0 +1,19 @@
+<div class="container-fluid top_nav">
+  <div class="row">
+    <div class="col-xs-12">
+      <ul class="topnav" id="top_nav_burg">
+        <li><a href="about.php">about</a></li>
+        <li><a href="family.php">family</a></li>
+        <li><a href="sundry.php">sundry</a></li>
+        <li><a href="words.php">words</a></li>
+        <li><a href="science.php">science</a></li>
+        <li><a href="fauna.php">fauna</a></li>
+        <li><a href="games.php">games</a></li>
+        <li><a href="index.php">home</a></li>
+        <li class="icon">
+          <a href="javascript:void(0);" onclick="hamburg()">&#9776;</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>

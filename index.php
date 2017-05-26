21847a3ab45b38dd81e3357b597097f18a9d9edc
Joel Harsevoort <bjoelh@gmail.com>
2017-05-26 16:32:01 -0400

    go

--- index.php
+++ index.php
@@ -26,22 +26,22 @@
   <hr class="perma_hr">
     <div class="column shutters">
       <div class="col-xs-2 shutt" id="games_shutter">
-        <a href="games.php"><div class="vert_text games_shutter"><span class="hidden-sm hidden-xs">games</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></span></div></a>
+        <a href="games.php"><div class="vert_text"><span class="hidden-sm hidden-xs">games</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></span></div></a>
       </div>
       <div class="col-xs-2 shutt" id="fauna_shutter">
-        <a href="fauna.php"><div class="vert_text">fauna</div></a>
+        <a href="fauna.php"><div class="vert_text"><span class="hidden-sm hidden-xs">fauna</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/fauna_icon.png" alt="fauna" style="width:48px;height:48px;"/></span></div></a>
       </div>
       <div class="col-xs-2 shutt" id="science_shutter">
-        <a href="science.php"><div class="vert_text">science</div></a>
+        <a href="science.php"><div class="vert_text"><span class="hidden-sm hidden-xs">science</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="science" style="width:48px;height:48px;"/></span></div></a>
       </div>
       <div class="col-xs-2 shutt" id="words_shutter">
-        <a href="words.php"><div class="vert_text">words</div></a>
+        <a href="words.php"><div class="vert_text"><span class="hidden-sm hidden-xs">words</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="words" style="width:48px;height:48px;"/></span></div></a>
       </div>
       <div class="col-xs-2 shutt" id="sundry_shutter">
-        <a href="sundry.php"><div class="vert_text">sundry</div></a>
+        <a href="sundry.php"><div class="vert_text"><span class="hidden-sm hidden-xs">sundry</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="sundry" style="width:48px;height:48px;"/></span></div></a>
       </div>
       <div class="col-xs-2 shutt" id="media_shutter">
-        <a href="media.php"><div class="vert_text">media</div></a>
+        <a href="media.php"><div class="vert_text"><span class="hidden-sm hidden-xs">media</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="media" style="width:48px;height:48px;"/></span></div></a>
       </div>
     </div>
   <script src="js/jquery-3.1.1.min.js"></script>

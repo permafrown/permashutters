d0af9b9a8d89fa070026c0d160b36e21c42e290b
Joel Harsevoort <bjoelh@gmail.com>
2017-06-07 17:24:54 -0400

    correcting shutter-menu on all pages

--- science.php
+++ science.php
@@ -23,25 +23,7 @@
   </div>
   <hr class="perma_hr">
   <div class="column shutters">
-    <div class="col-xs-2 shutt" id="games_shutter">
-      <a href="games.php"><div class="vert_text"><span class="hidden-sm hidden-xs">games</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-    <div class="col-xs-2 shutt" id="fauna_shutter">
-      <a href="fauna.php"><div class="vert_text"><span class="hidden-sm hidden-xs">fauna</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/fauna_icon.png" alt="fauna" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-    <div class="col-xs-2 shutt" id="science_shutter">
-      <a href="science.php"><div class="vert_text"><span class="hidden-sm hidden-xs">science</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/science_icon.png" alt="science" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-    <div class="col-xs-2 shutt" id="words_shutter">
-      <a href="words.php"><div class="vert_text"><span class="hidden-sm hidden-xs">words</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/words_icon.png" alt="words" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-    <div class="col-xs-2 shutt" id="sundry_shutter">
-      <a href="sundry.php"><div class="vert_text"><span class="hidden-sm hidden-xs">sundry</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/sundry_icon.png" alt="sundry" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-    <div class="col-xs-2 shutt" id="media_shutter">
-      <a href="media.php"><div class="vert_text"><span class="hidden-sm hidden-xs">media</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/media_icon.png" alt="media" style="width:48px;height:48px;"/></span></div></a>
-    </div>
-  </div>
+    <?php include_once 'includes/shutter_menu.php';?>
     <?php include_once 'includes/body_scripts.php';?>
 </body>
 

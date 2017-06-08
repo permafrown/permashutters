be81de175997c9b7c865dc3492924c15ea1768f8
Joel Harsevoort <bjoelh@gmail.com>
2017-06-08 14:27:58 -0400

    updated shutter menu php include for all pages

--- includes/shutter_menu.php
+++ includes/shutter_menu.php
@@ -1,5 +1,5 @@
 <div class="column shutters">
-  <div class="col-xs-2 shutt" id="games_shutter">
+  <div class="col-xs-2 shutt" id="games_shutter" style="background-image: url('./img/games_bg.jpg');">
     <a href="games.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">games</span>
@@ -9,7 +9,7 @@
         </div>
     </a>
   </div>
-  <div class="col-xs-2 shutt fauna_shutt" id="fauna_shutter">
+  <div class="col-xs-2 shutt fauna_shutt" id="fauna_shutter" style="background-image: url('./img/fauna_bg.jpg');">
     <a href="fauna.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">fauna</span>
@@ -19,7 +19,7 @@
         </div>
     </a>
   </div>
-  <div class="col-xs-2 shutt science_shutt" id="science_shutter">
+  <div class="col-xs-2 shutt science_shutt" id="science_shutter" style="background-image: url('./img/science_bg.png');">
     <a href="science.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">science</span>
@@ -29,7 +29,7 @@
         </div>
     </a>
   </div>
-  <div class="col-xs-2 shutt words_shutt" id="words_shutter">
+  <div class="col-xs-2 shutt words_shutt" id="words_shutter" style="background-image: url('./img/words_bg.jpg');">
     <a href="words.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">words</span>
@@ -39,7 +39,7 @@
         </div>
     </a>
   </div>
-  <div class="col-xs-2 shutt sundry_shutt" id="sundry_shutter">
+  <div class="col-xs-2 shutt sundry_shutt" id="sundry_shutter" style="background-image: url('./img/sundry_bg.jpg');">
     <a href="sundry.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">sundry</span>
@@ -49,7 +49,7 @@
         </div>
     </a>
   </div>
-  <div class="col-xs-2 shutt media_shutt" id="media_shutter">
+  <div class="col-xs-2 shutt media_shutt" id="media_shutter" style="background-image: url('./img/media_bg.jpg');">
     <a href="media.php">
         <div class="vert_text">
             <span class="hidden-sm hidden-xs">media</span>

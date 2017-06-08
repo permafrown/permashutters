239ce17f28ae2e841df3278103519a259cf2e1a1
Joel Harsevoort <bjoelh@gmail.com>
2017-06-08 14:23:47 -0400

    added glut of backgrounds for shutters & edited shutter menu to account for changes

--- index.php
+++ index.php
@@ -25,23 +25,65 @@
   <h2 class="submenu animated pulse" style="margin-bottom: 5%;">a collection of things i find interesting</h2>
   <hr class="perma_hr">
     <div class="column shutters">
-      <div class="col-xs-2 shutt" id="games_shutter">
-        <a href="games.php"><div class="vert_text"><span class="hidden-sm hidden-xs">games</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt" id="games_shutter" style="background-image: url('./img/games_bg.jpg');">
+        <a href="games.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">games</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
-      <div class="col-xs-2 shutt" id="fauna_shutter">
-        <a href="fauna.php"><div class="vert_text"><span class="hidden-sm hidden-xs">fauna</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/fauna_icon.png" alt="fauna" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt fauna_shutt" id="fauna_shutter" style="background-image: url('./img/fauna_bg.jpg');">
+        <a href="fauna.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">fauna</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/fauna_icon.png" alt="fauna" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
-      <div class="col-xs-2 shutt" id="science_shutter">
-        <a href="science.php"><div class="vert_text"><span class="hidden-sm hidden-xs">science</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/science_icon.png" alt="science" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt science_shutt" id="science_shutter" style="background-image: url('./img/science_bg.png');">
+        <a href="science.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">science</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/science_icon.png" alt="science" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
-      <div class="col-xs-2 shutt" id="words_shutter">
-        <a href="words.php"><div class="vert_text"><span class="hidden-sm hidden-xs">words</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/words_icon.png" alt="words" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt words_shutt" id="words_shutter" style="background-image: url('./img/words_bg.jpg');">
+        <a href="words.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">words</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/words_icon.png" alt="words" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
-      <div class="col-xs-2 shutt" id="sundry_shutter">
-        <a href="sundry.php"><div class="vert_text"><span class="hidden-sm hidden-xs">sundry</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/sundry_icon.png" alt="sundry" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt sundry_shutt" id="sundry_shutter" style="background-image: url('./img/sundry_bg.jpg');">
+        <a href="sundry.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">sundry</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/sundry_icon.png" alt="sundry" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
-      <div class="col-xs-2 shutt" id="media_shutter">
-        <a href="media.php"><div class="vert_text"><span class="hidden-sm hidden-xs">media</span><span class="hidden-md hidden-lg hidden-xl"><img src="img/media_icon.png" alt="media" style="width:48px;height:48px;"/></span></div></a>
+      <div class="col-xs-2 shutt media_shutt" id="media_shutter" style="background-image: url('./img/media_bg.jpg');">
+        <a href="media.php">
+            <div class="vert_text">
+                <span class="hidden-sm hidden-xs">media</span>
+                <span class="hidden-md hidden-lg hidden-xl">
+                    <img src="img/media_icon.png" alt="media" style="width:48px;height:48px;"/>
+                </span>
+            </div>
+        </a>
       </div>
     </div>
   <script src="js/jquery-3.1.1.min.js"></script>

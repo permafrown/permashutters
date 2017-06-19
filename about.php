2eb062d7d8d59e04a64cbd7c7155faa864f8227e
Joel Harsevoort <bjoelh@gmail.com>
2017-06-19 14:56:23 -0400

    monkeying with about parallax text

--- about.php
+++ about.php
@@ -31,8 +31,8 @@
                     data-anchor-target="#slide-1 h1";
                 >
                     <h1 class="title_centered"><small>permashutters</small>about</h1><br/>
-                    <span>permashutters is a place where i like to keep a variety of things that interest me<br/>
-                    since there are many things that do, i've tried to create some different spaces for different topics that tickle my fancy</span>
+                    <span>permashutters is a place where i like to keep a variety of things that interest me</span>
+                    <span>since there are many, i've created some different spaces for different topics that tickle my fancy</span>
                 </div>
             </div>
         </div>

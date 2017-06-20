e0046e9f63a54667292148c75a006ea34c8f52fd
joel <bjoelh@gmail.com>
2017-06-19 20:38:28 -0400

    removed parallax from about php and from css - rebuilding from scratch

---
+++ OLD/about_para_01.php
@@ -0,0 +1,144 @@
+<main>
+  <section id="slide-1" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-anchor-target="#slide-1";
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-center="bottom: 200px; opacity: 1;"
+                  data-top="bottom: 800px; opacity: 0;"
+                  data-anchor-target="#slide-1 p";
+              >
+                  <h1 class="title_centered"><small>permashutters</small>about</h1><br/>
+                  <p>permashutters is a place where i like to keep a variety of things that interest me</p>
+                  <p>since there are many, i've created some different spaces for different topics that tickle my fancy</p>
+                  <span>^^^^^scroll down to see more^^^^^</span>
+              </div>
+          </div>
+      </div>
+  </section>
+  <section id="slide-2" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-2"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-center="opacity: 1;"
+                  data-center-top="opacity: 0;"
+                  data--100-bottom="opacity: 0;"
+                  data-anchor-target="#slide-2"
+              >
+                  <h3><a href="games.php">games </a></h3><br/>
+                  <a href="games.php"><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></a><br/>
+                  <h3 class="text_centered">starting with tabletop games, then from nintendo to playstation to pc,including everything from FPS to RPG to sports and side-scrollers, my tastes for gaming run wide and far...</h3>
+              </div>
+          </div>
+      </div>
+  </section>
+  <section id="slide-3" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-3"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+              data--50-bottom="opacity: 0;"
+              data--200-bottom="opacity: 1;"
+              data-center="opacity: 1"
+              data-200-top="opacity: 0"
+              data-anchor-target="#slide-3 h3"
+          >
+                  <h3>fauna</h3>
+              </div>
+          </div>
+
+      </div>
+  </section>
+  <section id="slide-4" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-4"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-bottom-top="opacity: 0"
+                  data-25p-top="opacity: 0"
+                  data-top="opacity: 1"
+                  data-anchor-target="#slide-4"
+              >
+                  <h3>science FTW</h3>
+              </div>
+          </div>
+
+      </div>
+  </section>
+  <section id="slide-5" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-5"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-bottom-top="opacity: 0"
+                  data-25p-top="opacity: 0"
+                  data-top="opacity: 1"
+                  data-anchor-target="#slide-5"
+              >
+                  <h3>words is words</h3>
+              </div>
+          </div>
+
+      </div>
+  </section>
+  <section id="slide-6" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-6"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-bottom-top="opacity: 0"
+                  data-25p-top="opacity: 0"
+                  data-top="opacity: 1"
+                  data-anchor-target="#slide-6"
+              >
+                  <h3>sundry is fundry</h3>
+              </div>
+          </div>
+
+      </div>
+  </section>
+  <section id="slide-7" class="homeSlide">
+      <div class="bcg"
+          data-center="background-position: 50% 0px;"
+          data-top-bottom="background-position: 50% -100px;"
+          data-bottom-top="background-position: 50% 100px;"
+          data-anchor-target="#slide-7"
+      >
+          <div class="hsContainer">
+              <div class="hsContent"
+                  data-bottom-top="opacity: 0"
+                  data-25p-top="opacity: 0"
+                  data-top="opacity: 1"
+                  data-anchor-target="#slide-7"
+              >
+                  <h3>media medium medius</h3>
+              </div>
+          </div>
+
+      </div>
+  </section>
+</main>

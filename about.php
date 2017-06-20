c0ea8ac0d1e810ef6b46ba4893c0d6a4898009f9
joel <bjoelh@gmail.com>
2017-06-19 21:15:07 -0400

    correction to about page about section, removing games and adding about

--- about.php
+++ about.php
@@ -16,9 +16,9 @@
 </head>
 <body>
   <section class="para_page" id="about_para_page">
-    <div class="para_bg" id="games_para_bg">
-      <div class="para_content_container" id="games_para_content_container">
-        <div class="para_content" id="games_para_content">
+    <div class="para_bg" id="about_para_bg">
+      <div class="para_content_container" id="about_para_content_container">
+        <div class="para_content" id="about_para_content">
           <h1 class="title_centered" style="margin-top: 10%;"><small>permashutters</small>about</h1>
           <div class="container-fluid about_content">
             <p>permashutters is a place where i like to keep a variety of things that interest me</p>

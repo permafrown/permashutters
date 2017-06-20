1b57d3bba6cda28018a1695f9c0e50f7c22bf8f3
Harsevoort <jharsevoort@clac.ca>
2017-06-20 14:48:24 -0400

    asdf

--- about.php
+++ about.php
@@ -44,8 +44,9 @@
     >
       <div class="para_content" id="games_para_content"
         data-center="opacity: 1;"
-        data-center-top="opacity: 0;"
+        data-center-top="opacity: 0.5;"
         data--100-bottom="opacity: 0;"
+        data--100-bottom="left: -500px;"
         data-anchor-target="#games_para_page"
       >
         <h3><a href="games.php">games </h3>

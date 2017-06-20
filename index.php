6cdc78c3d338f0c929fefd99edcc214455639aac
Harsevoort <jharsevoort@clac.ca>
2017-06-20 13:24:23 -0400

    updating font set; adjusting font of para_content

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <link href="https://fonts.googleapis.com/css?family=Bevan|Cinzel|EB+Garamond|Lato|Libre+Baskerville|Overlock|Poiret+One|Righteous|Roboto+Condensed|Source+Sans+Pro|Tangerine|Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bevan|Cinzel|Cinzel+Decorative|EB+Garamond|Lato|Libre+Baskerville|Montserrat|Overlock|Overlock+SC|Poiret+One|Roboto+Condensed|Source+Code+Pro|Source+Sans+Pro|Source+Serif+Pro|Tangerine" rel="stylesheet">
     <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">

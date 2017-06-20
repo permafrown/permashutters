1b3f8c3dcf2fa01ccc5421d2fd9413cd7d959863
joel <bjoelh@gmail.com>
2017-06-19 21:34:55 -0400

    created about_old from old about desc menu_list

--- about.php
+++ about.php
@@ -93,33 +93,6 @@
     </div>
   </section>
 
-
-  <ul class="about_desc_list">
-      <li><h3><a href="games.php">games </h3><img src="img/games_icon.png" alt="games" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>starting with tabletop games, then from nintendo to playstation to pc,including everything from FPS to RPG to sports and side-scrollers, my tastes for gaming run wide and far...</li>
-      <li><h3><a href="fauna.php">fauna </h3><img src="img/fauna_icon.png" alt="fauna" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>i'll admit that 'fauna' is only half the story here since flora certainly play a role as well -- i love all the created things... ALL THE THINGS!</li>
-      <li><h3><a href="science.php">science </h3><img src="img/science_icon.png" alt="science" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>and math and physics and stuff... these are cool concepts or discoveries that blow my mind</li>
-      <li><h3><a href="words.php">words </h3><img src="img/words_icon.png" alt="words" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>i love language... more than lamp</li>
-      <li><h3><a href="sundry.php">sundry </h3><img src="img/sundry_icon.png" alt="sundry" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>a place to drop other random things i think are neat but don't fit the other categories</li>
-      <li><h3><a href="media.php">media </h3><img src="img/media_icon.png" alt="media" style="width:48px;height:48px;"/></a></li>
-      <li>&nbsp;</li>
-      <li>films & music & art</li>
-  </ul>
-  </div>
-  <div class="container-fluid">
-    <div class="col-xs-12">
-      <a href="index.php"><img style="margin-bottom: 5%; margin-top: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
-    </div>
-  </div>
   <?php include_once 'includes/body_scripts.php';?>
 </body>
 

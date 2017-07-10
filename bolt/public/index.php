a6cb53629e27173f81b5df869746d21f0a640aa5
Joel Harsevoort <jharsevoort@clac.ca>
2017-07-10 14:57:05 -0400

    asdf

--- bolt/public/index.php
+++ bolt/public/index.php
@@ -1,14 +1,4 @@
 <?php
-// Assuming this is in public/index.php and Bolt is located in vendor/bolt/bolt/
-$map = [
-    '/another' => new AnotherApplication(),
-    '/blog'    => require __DIR__ . '/../vendor/bolt/bolt/app/bootstrap.php'
-];
-$app = (new Stack\Builder())
-    ->push('Stack\UrlMap', $map)
-    ->resolve($app);
-
-Stack\run($app);
 
 /** @var Silex\Application|false $app */
 $app = require __DIR__ . '/../vendor/bolt/bolt/app/web.php';

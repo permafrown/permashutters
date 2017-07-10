1c13da085adb082d7a847ca10b5c6b2c37a6c3cd
Joel Harsevoort <jharsevoort@clac.ca>
2017-07-10 14:30:56 -0400

    adding TEST green to all page heading

--- media.php
+++ media.php
@@ -6,7 +6,7 @@
   <title>media | permashutters</title>
 </head>
 <body>
-  <h1 class="title_centered" style="margin-top: 10%;"><small>permashutters</small>media</h1>
+  <h1 class="title_centered" style="margin-top: 10%; color: #0F0;"><small>permashutters</small>media</h1>
   <div class="container-fluid">
     <div class="col-xs-12">
       <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>

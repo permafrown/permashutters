4fedfec2edff977f7520a2354e5cb6799cf6af1f
Joel Harsevoort <jharsevoort@clac.ca>
2017-07-13 11:25:24 -0400

    added bootstrap cdn to body_scripts and head; testing cards on games

--- games.php
+++ games.php
@@ -13,7 +13,7 @@
     </div>
   </div>
   <hr class="perma_hr">
-  <div class="cushycms container-fluid shutt_page_content">
+  <div class="container-fluid shutt_page_content">
     <div class="button shutt_butt" id="button001">
         <a href="https://www.youtube.com/watch?v=Kdaoe4hbMso">
             <img src="https://i.ytimg.com/vi/PI-1KTy0pOA/maxresdefault.jpg" alt="FC5 announce trailer"/>
@@ -23,6 +23,48 @@
         <a href="https://far-cry.ubisoft.com/game/en-ca/home/">
             <img src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/search-thumbnail/fc5-wideart-table-search_thumnail_290060.jpg" alt="FC5 Official Site"/>
         </a>
+    </div>
+    <div class="row">
+        <div class="col-xs-6">
+            <div class="card" style="width: 25vw;">
+                <div class="card-header">perma-featured</div>
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">some quick text to build on the card title and blah blah blah</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">item 001</li>
+                    <li class="list-group-item">item 002</li>
+                    <li class="list-group-item">item 003</li>
+                </ul>
+                <div class="card-block">
+                    <a href="#" class="card-link">card link</a>
+                    <a href="#" class="card-link">un autre link</a>
+                </div>
+                <div class="card-footer text-muted">this has been perma-featured</div>
+            </div>
+        </div>
+        <div class="col-xs-6">
+            <div class="card text-center" style="width: 25vw;">
+                <div class="card-header">perma-featured</div>
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-block">
+                    <h4 class="card-title">Card title</h4>
+                    <p class="card-text">some quick text to build on the card title and blah blah blah</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">item 001</li>
+                    <li class="list-group-item">item 002</li>
+                    <li class="list-group-item">item 003</li>
+                </ul>
+                <div class="card-block">
+                    <a href="#" class="card-link">card link</a>
+                    <a href="#" class="card-link">un autre link</a>
+                </div>
+                <div class="card-footer text-muted">this has been perma-featured</div>
+            </div>
+        </div>
     </div>
   </div>
   <hr class="perma_hr">

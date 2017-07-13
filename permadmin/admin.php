d688240e715520e6db9946c5c8814b5b5240931b
permafrown <bjoelh@gmail.com>
2017-07-13 16:09:46 -0400

    added admin section

---
+++ permadmin/admin.php
@@ -0,0 +1,31 @@
+<!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once 'includes/head.php';?>
+    <title>TESTpermashutters</title>
+</head>
+<body>
+
+<!-- MAIN TITLE -->
+  <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;">permaadmin</h1>
+<!-- END MAIN TITLE -->
+
+<!-- SCRIPTS -->
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery-3.1.1.min.js"><\/script>')</script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/modernizr-2.7.1.min.js"></script>
+  <script src="js/imagesloaded.js"></script>
+  <script src="js/skrollr.js"></script>
+  <script src="js/_main.js"></script>
+  <script src="js/enquire.min.js"></script>
+<!-- END SCRIPTS -->
+
+</body>
+
+<footer>
+    <?php include_once 'includes/botnav.php';?>
+</footer>
+
+</html>

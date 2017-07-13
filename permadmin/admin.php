752106205b57350ec0355fd29aa0844bbc494e5b
permafrown <bjoelh@gmail.com>
2017-07-13 16:12:21 -0400

    config admin section

--- permadmin/admin.php
+++ permadmin/admin.php
@@ -3,13 +3,21 @@
 <html lang="en">
 <head>
     <?php include_once 'includes/head.php';?>
-    <title>TESTpermashutters</title>
+    <title>TESTpermashuttersADMIN</title>
 </head>
 <body>
 
 <!-- MAIN TITLE -->
-  <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;">permaadmin</h1>
+  <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;">permADMIN</h1>
 <!-- END MAIN TITLE -->
+
+<!-- LARGE SCREEN SUBMENU -->
+  <h2 class="submenu hidden-sm-down"><a href="games.php">games</a> | <a href="fauna.php">fauna</a> | <a href="science.php">science</a> | <a href="words.php">words</a> | <a href="sundry.php">sundry</a> | <a href="media.php">media</a> | <a href="about.php">about</a></h2>
+<!-- END LARGE SCREEN SUBMENU -->
+
+<!-- SMALL SCREEN SUBMENU -->
+    <h2 class="submenu hidden-md-up"><a href="about.php">about</a></h2>
+<!-- END SMALL SCREEN SUBMENU -->
 
 <!-- SCRIPTS -->
   <script src="js/jquery-3.1.1.min.js"></script>

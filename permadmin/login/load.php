db7145e39fb5bf0d1943e9acebdd2af0e6696e8f
permafrown <bjoelh@gmail.com>
2017-07-14 14:32:13 -0400

    adding login functionality

--- permadmin/login/load.php
+++ permadmin/login/load.php
@@ -1,6 +1,6 @@
 <?php
 	//Load all the required files in order
-	require_once(dirname(__FILE__) . '/login/config.php');
-	require_once(dirname(__FILE__) . '/login/l_includes/db.php');
-	require_once(dirname(__FILE__) . '/login/l_includes/class.php');
+	require_once(dirname(__FILE__) . '/config.php');
+	require_once(dirname(__FILE__) . '/l_includes/db.php');
+	require_once(dirname(__FILE__) . '/l_includes/class.php');
 ?>

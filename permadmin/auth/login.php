af33bc3fea872be05f3f48dd05228e96a147a3ec
permafrown <bjoelh@gmail.com>
2017-07-14 15:00:26 -0400

    adding auth

--- permadmin/auth/login.php
+++ permadmin/auth/login.php
@@ -7,7 +7,14 @@
 
 <body>
 
-    <h1>permaLogin</h1>
+    <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;"><small>perma</small>LOGIN</h1>
+
+    <form action="login.php" method="POST">
+        <input type="text" placeholder="enter username" name="u/n">
+        <input type="password" placeholder="enter password" name="p/w">
+
+        <input type="submit">
+
 
     <?php include_once('../../includes/body_scripts.php'); ?>
 </body>

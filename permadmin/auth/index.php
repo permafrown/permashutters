75301c52d613d2ac68e7b7e22da11b7468a88fd6
permafrown <bjoelh@gmail.com>
2017-07-14 15:00:26 -0400

    adding auth

--- permadmin/auth/index.php
+++ permadmin/auth/index.php
@@ -2,14 +2,15 @@
 <html>
 <head>
     <?php include_once('../../includes/head.php'); ?>
-    <title>permaWelcome</title>
+    <title>permaLOGIN</title>
 </head>
 
 <body>
 
-    <h1>welcome to permaLogin</h1>
+    <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;"><small>perma</small>WELCOME</h1>
     <a href="login.php">login</a> or
     <a href="reg.php">register</a>
 
+    <?php include_once('../../includes/body_scripts.php'); ?>
 </body>
 </html>
--- permadmin/auth/reg.php
+++ permadmin/auth/reg.php
@@ -7,7 +7,7 @@
 
 <body>
 
-    <h1>permaREG</h1>
+    <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;"><small>perma</small>REG</h1>
 
     <?php include_once('../../includes/body_scripts.php'); ?>
 </body>

eec0f6a4697aa082288674871a8c16bc7b4721d2
permafrown <bjoelh@gmail.com>
2017-07-14 14:48:26 -0400

    adding auth

---
+++ permadmin/auth/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include_once('../../includes/head.php'); ?>
+    <title>permaWelcome</title>
+</head>
+
+<body>
+    <h1>welcome to permaLogin</h1>
+
+</body>
+</html>

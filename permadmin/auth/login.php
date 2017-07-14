688252fcbe04f140fdcfe81e1939c5284d5e1b0b
permafrown <bjoelh@gmail.com>
2017-07-14 16:01:53 -0400

    adding auth

--- permadmin/auth/login.php
+++ permadmin/auth/login.php
@@ -16,7 +16,6 @@
     <link href="https://fonts.googleapis.com/css?family=Bevan|Cinzel|Cinzel+Decorative|EB+Garamond|Lato|Libre+Baskerville|Montserrat|Overlock|Overlock+SC|Poiret+One|Roboto+Condensed|Source+Code+Pro|Source+Sans+Pro|Source+Serif+Pro|Tangerine" rel="stylesheet">
     <link rel="shortcut icon" href="../../favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../css/animate-master/animate.css">
     <link rel="stylesheet" href="../../css/shutters.css">
     <link rel="stylesheet" href="../../css/shutt_page_content.css">
     <link rel="stylesheet" href="../../css/main.css">

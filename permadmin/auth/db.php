a207a00a039b10b6f7fa4ec60caa6bb780c2064b
joel <bjoelh@gmail.com>
2017-07-14 21:22:36 -0400

    asdf

--- permadmin/auth/db.php
+++ permadmin/auth/db.php
@@ -5,10 +5,16 @@ $username = 'shutt_dbU';
 $password = 'Joplin098*';
 $database = 'testshutters_DB';
 
-try{
-    $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password)
-} catch(PDOException $e){
-    die( "connection failed: " . $e->getMessage());
-}
+// try{
+//     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password)
+// } catch(PDOException $e){
+//     die( "connection failed: " . $e->getMessage());
+// }
+
+
+$mysqli = new mysqli($server, $username, $password, $database);
+$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+$row = $result->fetch_assoc();
+echo htmlentities($row['_message']);
 
 ?>

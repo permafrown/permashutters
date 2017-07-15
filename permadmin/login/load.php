26cc0f76520a429d8de33ae428881523fb555191
joel <bjoelh@gmail.com>
2017-07-14 23:12:25 -0400

    asdf

--- permadmin/login/load.php
+++ permadmin/login/load.php
@@ -3,7 +3,7 @@
 	//require_once(dirname(__FILE__) . './permadmin/login//config.php');
 	//require_once(dirname(__FILE__) . './permadmin/login//l_includes/db.php');
 	//require_once(dirname(__FILE__) . './permadmin/login//l_includes/class.php');
-	include_once(dirname(__FILE__) . './permadmin/login//config.php');
-	include_once(dirname(__FILE__) . './permadmin/login//l_includes/db.php');
-	include_once(dirname(__FILE__) . './permadmin/login//l_includes/class.php');
+	include_once('./permadmin/login//config.php');
+	include_once('./permadmin/login//l_includes/db.php');
+	include_once('./permadmin/login//l_includes/class.php');
 ?>

740f0cc3ba023f0a29d97d7377629352a1f22973
joel <bjoelh@gmail.com>
2017-07-14 22:23:31 -0400

    asdf

--- permadmin/login/l_includes/db.php
+++ permadmin/login/l_includes/db.php
@@ -1,5 +1,5 @@
 <?php
-require_once('./permadmin/login/config.php')
+require_once('./permadmin/login/config.php');
 // Our database class
 if(!class_exists('shuttDatabase')){
 	class shuttDatabase {

1aaa6a5924f7694035b858a546305910adb6f09e
joel <bjoelh@gmail.com>
2017-07-14 21:40:52 -0400

    asdf

--- permadmin/auth/db.php
+++ permadmin/auth/db.php
@@ -5,30 +5,30 @@ $username = 'shutt_dbU';
 $password = 'Joplin098*';
 $database = 'testshutters_DB';
 
-// try{
-//      $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password)
-// } catch(PDOException $e){
-//     die( "connection failed: " . $e->getMessage());
+try{
+     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password)
+} catch(PDOException $e){
+    die( "connection failed: " . $e->getMessage());
+}
+
+//
+// $mysqli = new mysqli($server, $username, $password, $database);
+// $result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+// $row = $result->fetch_assoc();
+// echo htmlentities($row['_message']);
+//
+//
+// $link = mysql_connect($server, $username, $password);
+// if (!$link) {
+//     die('Could not connect: ' . mysql_error());
 // }
-
-
-$mysqli = new mysqli($server, $username, $password, $database);
-$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
-$row = $result->fetch_assoc();
-echo htmlentities($row['_message' + '    |     ']);
-
-
-$link = mysql_connect($server, $username, $password);
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-if (!mysql_select_db($database)) {
-    die('Could not select database: ' . mysql_error());
-}
-$result = mysql_query('SELECT user_name FROM shutt_users');
-if (!$result) {
-    die('Could not query:' . mysql_error());
-}
-echo mysql_result($result, 0); // outputs first user's user_ID
+// if (!mysql_select_db($database)) {
+//     die('Could not select database: ' . mysql_error());
+// }
+// $result = mysql_query('SELECT user_name FROM shutt_users');
+// if (!$result) {
+//     die('Could not query:' . mysql_error());
+// }
+// echo mysql_result($result, 0); // outputs first user's user_ID
 
 ?>

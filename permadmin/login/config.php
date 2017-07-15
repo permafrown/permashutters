5948966cfb4657e20b8066b5ee72480c8e61b963
joel <bjoelh@gmail.com>
2017-07-14 20:48:43 -0400

    asdf

--- permadmin/login/config.php
+++ permadmin/login/config.php
@@ -13,7 +13,7 @@ define('DB_NAME', 'testshutters_DB');
 /* DB Username
  * Enter the username of the user with access to the database below.
  */
-define('DB_USER', 'shutt_dbU@localhost:3306');
+define('DB_USER', 'shutt_dbU');
 
 /* DB Password
  * Enter the above user's password below.

43c19ac5195afc341cb69ff0151d58ce0adca261
joel <bjoelh@gmail.com>
2017-07-14 21:33:25 -0400

    asdf

--- permadmin/auth/db.php
+++ permadmin/auth/db.php
@@ -17,4 +17,18 @@ $result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL
 $row = $result->fetch_assoc();
 echo htmlentities($row['_message']);
 
+
+$link = mysql_connect($server, $username, $password);
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+if (!mysql_select_db($database)) {
+    die('Could not select database: ' . mysql_error());
+}
+$result = mysql_query('SELECT user_ID FROM shutt_users');
+if (!$result) {
+    die('Could not query:' . mysql_error());
+}
+echo mysql_result($result, 0); // outputs first user's user_ID
+
 ?>

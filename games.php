8a52e6525096a7d673c2378f560225871112c7d3
permafrown <bjoelh@gmail.com>
2017-07-17 11:54:02 -0400

    beautifying many things; updating PDO

--- games.php
+++ games.php
@@ -1,19 +1,21 @@
 <!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php include_once 'includes/head.php';?>
-  <title>games | permashutters</title>
+    <title>games | permashutters</title>
 </head>
+
 <body>
-  <h1 class="title_centered" style="margin-top: 10%; color: #0F0;"><small>permashutters</small>games</h1>
-  <div class="container-fluid">
-    <div class="col-xs-12">
-      <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
+    <h1 class="title_centered" style="margin-top: 10%; color: #0F0;"><small>permashutters</small>games</h1>
+    <div class="container-fluid">
+        <div class="col-xs-12">
+            <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
+        </div>
     </div>
-  </div>
-  <hr class="perma_hr">
-  <div class="container-fluid d-inline-flex shutt_page_content">
+    <hr class="perma_hr">
+    <div class="container-fluid d-inline-flex shutt_page_content">
         <!-- <div class="button shutt_butt" id="button001">
             <a href="https://www.youtube.com/watch?v=Kdaoe4hbMso">
                 <img src="https://i.ytimg.com/vi/PI-1KTy0pOA/maxresdefault.jpg" alt="FC5 announce trailer"/>
@@ -28,7 +30,7 @@
             <div class="col-xs-5">
                 <div class="card">
                     <div class="card-header">perma-featured</div>
-                    <img class="card-img-top" src="https://i.ytimg.com/vi/PI-1KTy0pOA/maxresdefault.jpg" alt="FC5 Announce Trailer"/>
+                    <img class="card-img-top" src="https://i.ytimg.com/vi/PI-1KTy0pOA/maxresdefault.jpg" alt="FC5 Announce Trailer" />
                     <div class="card-block">
                         <h3 class="card-title">far cry 5 announce trailer</h3>
                         <p class="card-text">can't wait for this game</p>
@@ -48,7 +50,8 @@
             <div class="col-xs-5">
                 <div class="card shutt_card">
                     <div class="card-header">perma-featured</div>
-                    <img class="card-img-top" src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/search-thumbnail/fc5-wideart-table-search_thumnail_290060.jpg" alt="FC5 Official Site"/>
+                    <img class="card-img-top" src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/search-thumbnail/fc5-wideart-table-search_thumnail_290060.jpg"
+                        alt="FC5 Official Site" />
                     <div class="card-block">
                         <h3 class="card-title">far cry 5 official site</h3>
                         <p class="card-text">check it out to follow updates, etc.</p>
@@ -66,8 +69,8 @@
                 </div>
             </div>
         </div>
-  </div>
-  <hr class="perma_hr">
+    </div>
+    <hr class="perma_hr">
     <?php include_once 'includes/shutter_menu.php';?>
     <?php include_once 'includes/body_scripts.php';?>
 </body>

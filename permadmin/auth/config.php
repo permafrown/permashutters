3d4c8048cd430a7b53b7cbae0bc1ba2ac821d1d1
permafrown <bjoelh@gmail.com>
2017-07-18 14:15:16 -0400

    trying new PDO

---
+++ permadmin/auth/config.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+// Define database
+define('dbhost', 'localhost');
+define('dbuser', 'shutt_dbU');
+define('dbpass', 'Joplin098*');
+define('dbname', 'testshutters_DB');
+
+// Connecting database
+try {
+	$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+?>
--- permadmin/login/load.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//Load all the required files in order
-	require_once(dirname(__FILE__) . '/config.php');
-	require_once(dirname(__FILE__) . '/j_includes/db.php');
-	require_once(dirname(__FILE__) . '/j_includes/class.php');
-?>

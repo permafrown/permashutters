9d11eae7f2b65d2ef4a26bd1d0d8faa7b707c8bf
permafrown <bjoelh@gmail.com>
2017-07-18 15:20:07 -0400

    trying new PDO

--- includes/head.php
+++ includes/head.php
@@ -4,11 +4,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 <link href="https://fonts.googleapis.com/css?family=Bevan|Cinzel|Cinzel+Decorative|EB+Garamond|Lato|Libre+Baskerville|Montserrat|Overlock|Overlock+SC|Poiret+One|Roboto+Condensed|Source+Code+Pro|Source+Sans+Pro|Source+Serif+Pro|Tangerine"
     rel="stylesheet">
-<link rel="shortcut icon" href="favicon.ico">
+<link rel="shortcut icon" href="/favicon.ico">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"
     crossorigin="anonymous">
 <!--<link rel="stylesheet" href="css/bootstrap-theme.min.css">-->
 <!--<link rel="stylesheet" href="css/animate-master/animate.css">-->
-<link rel="stylesheet" href="css/shutters.css">
-<link rel="stylesheet" href="css/shutt_page_content.css">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="/css/shutters.css">
+<link rel="stylesheet" href="/css/shutt_page_content.css">
+<link rel="stylesheet" href="/css/main.css">

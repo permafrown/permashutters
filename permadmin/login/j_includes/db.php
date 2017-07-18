422c5cef79d42234419d3bb5efb7e85652c781b2
permafrown <bjoelh@gmail.com>
2017-07-18 13:18:35 -0400

    go

--- permadmin/login/j_includes/db.php
+++ permadmin/login/j_includes/db.php
@@ -31,20 +31,35 @@ if(!class_exists('shuttDatabase')){
 		 *
 		 * @uses the constants defined in config.php
 		 */
-		function connect() {
-			$link = mysql_connect('localhost', DB_USER, DB_PASS);
+		// function connect() {
+		// 	$link = mysql_connect('localhost', DB_USER, DB_PASS);
+        //
+		// 	if (!$link) {
+		// 		die('Could not connect: ' . mysql_error());
+		// 	}
+        //
+		// 	$db_selected = mysql_select_db(DB_NAME, $link);
+        //
+		// 	if (!$db_selected) {
+		// 		die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+		// 	}
+		// }
 
-			if (!$link) {
-				die('Could not connect: ' . mysql_error());
-			}
+        function connect() {
+            try {
+            $conn = new PDO("mysql:host=($db_host);dbname=($db_name)", DB_USER, DB_PASS);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $query = $conn->query('SELECT user_name, user_login FROM shutt_users');
 
-			$db_selected = mysql_select_db(DB_NAME, $link);
+            while($row = $query->fetch(PDO::FETCH_OBJ)) {
+                $results[] = $row;
+            }
 
-			if (!$db_selected) {
-				die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
-			}
-		}
-
+            print_r($results);
+        } catch(PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
+        }
 		/**
 		 * Clean the array using mysql_real_escape_string
 		 *

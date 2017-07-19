e1b80a95956c200908ff10c6fe485b0045c6d226
permafrown <bjoelh@gmail.com>
2017-07-19 15:52:05 -0400

    go

--- permadmin/index.php
+++ permadmin/index.php
@@ -25,7 +25,17 @@
 
 <h2 class="submenu">
     <?php
-    echo ($_SESSION['ulogin'] . ' | ' . date(DATE_RFC7231) . ' | america/toronto');
+    echo ($_SESSION['ulogin']);
+    ?>
+</h2>
+<h2 class="submenu">
+    <?php
+    echo date(DATE_RFC7231);
+    ?>
+</h2>
+<h2 class="submenu">
+    <?php
+    echo 'america/toronto';
     ?>
 </h2>
 <h2 class="submenu">

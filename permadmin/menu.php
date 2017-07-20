2d45cd03adf0ecb0cc38ff9f371d7067d341c65d
permafrown <bjoelh@gmail.com>
2017-07-20 16:57:04 -0400

    go

--- permadmin/menu.php
+++ permadmin/menu.php
@@ -1,5 +1,5 @@
 <h1>admin menu</h1>
-<ul class='adminmenu'>
+<ul class='admin_menu'>
 	<li><a href='index.php'>menu</a></li>
 	<li><a href='users.php'>users</a></li>
 	<li><a href="../" target="_blank">site</a></li>

3a4608876c061786a036ec50a8df86167042c4fa
joel <bjoelh@gmail.com>
2017-07-23 14:07:31 -0400

    'go'

--- permadmin/menu.php
+++ permadmin/menu.php
@@ -3,7 +3,7 @@
 	<li><a href='index.php'>menu</a></li>
 	<!--<li><a href='users.php'>users</a></li>-->
 	<li><a href="../" target="_blank">site</a></li>
-	<li><a href='logout.php'>logout</a></li>
+	<li><a href='auth/logout.php'>logout</a></li>
 </ul>
 <div class='clear'></div>
 <hr />

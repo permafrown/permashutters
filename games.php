e57b34960d47576ab46230eb19f921c3a45016f1
joel <bjoelh@gmail.com>
2017-07-23 14:15:02 -0400

    'go'

--- games.php
+++ games.php
@@ -28,25 +28,7 @@
             </a>
         </div> -->
 
-        <?php
-          try {
 
-            $stmt = $connect->query('SELECT postID, postTitle, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
-            while($row = $stmt->fetch()){
-
-              echo '<div>';
-                echo '<h1><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h1>';
-                echo '<p>Posted on '.date('jS M Y H:i:s', strtotime($row['postDate'])).'</p>';
-                echo '<p>'.$row['postDesc'].'</p>';
-                echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a></p>';
-              echo '</div>';
-
-            }
-
-          } catch(PDOException $e) {
-              echo $e->getMessage();
-          }
-        ?>
 
         <?php
         echo '<div class="row justify-content-center align-items-center">';

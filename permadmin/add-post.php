341a6b11ee505caa1b7ebdaf38d6b83a6c21fc37
permafrown <bjoelh@gmail.com>
2017-08-01 17:04:48 -0400

    go

--- permadmin/add-post.php
+++ permadmin/add-post.php
@@ -122,19 +122,10 @@ if(empty($_SESSION['ulogin']))
         <p><label>Category</label><br />
 		<input type='text' name='postCat' value='<?php echo $_POST['postCat'];?>'></p>
 
-        <p><label>category</label><br />
+        <p><label>category</label></p>
         <select name="postCat" id="postCat" class="form-control">
             <!-- NEED FIX HERE -->
         </select>
-
-                while ( $row = $connect->fetch() )
-                {
-                   echo '<option value="'.$row['postCat'].'">'.$row['postCat'].'</option>';
-                }
-
-                echo '</select>';
-            ?>
-        </p>
 
 		<p><label>Brief Description | 300 words</label><br />
 		<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>

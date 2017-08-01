95cfb992dbf26114294717701203228c09ddd165
permafrown <bjoelh@gmail.com>
2017-08-01 12:51:06 -0400

    adding normalize.css CDN to head.php

--- games.php
+++ games.php
@@ -17,38 +17,7 @@
     </div>
     <hr class="perma_hr">
     <div class="container-fluid shutt_page_content">
-        <div class="row d-inline-flex">
-        <!-- <div class="row justify-content-center align-items-center"> -->
-            <!-- <div class="col-xs-5"> -->
 
-            <?php
-              try {
-
-                $stmt = $connect->query('SELECT postID, postTitle, postImg, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
-                while($row = $stmt->fetch()){
-                    echo '<div class="col">';
-                        echo '<div class="card">';
-                        echo '<div class="card-header">perma-featured</div>';
-                        echo '<img class="card-img-top" src="" alt="'.$row['postTitle'].' image" />';
-                          echo '<div class="card-block">';
-                            echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
-                            echo '<p class="card-text">'.$row['postDesc'].'</p>';
-                          echo '</div>';
-                    echo '<div class="card-block">';
-                      echo '<a href="#" class="card-link">'.$row['postTitle'].'</a>';
-                      echo '<a href="#" class="card-link">'.$row['postID'].'</a>';
-                    echo '</div>';
-                    echo '<div class="card-footer text-muted">this has been perma-featured</div>';
-                    echo '</div>';
-                    echo '</div>';
-                }
-              } catch(PDOException $e) {
-                  echo $e->getMessage();
-              }
-            ?>
-
-            <!-- </div> -->
-        </div>
     </div>
     <hr class="perma_hr">
 

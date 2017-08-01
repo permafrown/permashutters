6ba72ac323ca597614ed374396bc474d5ebf871f
permafrown <bjoelh@gmail.com>
2017-08-01 09:23:50 -0400

    go

--- games.php
+++ games.php
@@ -20,76 +20,30 @@
         <div class="row justify-content-center align-items-center">
             <div class="col-xs-5">
 
-        <?php
-          try {
+            <?php
+              try {
 
-            $stmt = $connect->query('SELECT postID, postTitle, postImg, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
-            while($row = $stmt->fetch()){
-                    echo '<div class="card">';
-                    echo '<div class="card-header">perma-featured</div>';
-                    echo '<img class="card-img-top" src="" alt="'.$row['postTitle'].' image" />';
-                      echo '<div class="card-block">';
-                        echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
-                        echo '<p class="card-text">'.$row['postDesc'].'</p>';
-                      echo '</div>';
-                echo '<div class="card-block">';
-                  echo '<a href="#" class="card-link">'.$row['postTitle'].'</a>';
-                  echo '<a href="#" class="card-link">'.$row['postID'].'</a>';
-                echo '</div>';
-                echo '<div class="card-footer text-muted">this has been perma-featured</div>';
-                echo '</div>';
-            }
-          } catch(PDOException $e) {
-              echo $e->getMessage();
-          }
-        ?>
-
-            </div>
-        </div>
-
-        <?php
-        echo '<div class="row justify-content-center align-items-center">';
-            echo '<div class="col-xs-5">';
-                echo '<div class="card">';
-                    echo '<div class="card-header">perma-featured</div>';
-                    echo '<img class="card-img-top" src="https://i.ytimg.com/vi/PI-1KTy0pOA/maxresdefault.jpg" alt="FC5 Announce Trailer" />';
+                $stmt = $connect->query('SELECT postID, postTitle, postImg, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
+                while($row = $stmt->fetch()){
+                        echo '<div class="card">';
+                        echo '<div class="card-header">perma-featured</div>';
+                        echo '<img class="card-img-top" src="" alt="'.$row['postTitle'].' image" />';
+                          echo '<div class="card-block">';
+                            echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
+                            echo '<p class="card-text">'.$row['postDesc'].'</p>';
+                          echo '</div>';
                     echo '<div class="card-block">';
-                        echo '<h3 class="card-title">far cry 5 announce trailer</h3>';
-                        echo '<p class="card-text">can\'t wait for this game</p>';
-                    echo '</div>';
-                    echo '<ul class="list-group list-group-flush">';
-                        echo '<li class="list-group-item">item 001</li>';
-                        echo '<li class="list-group-item">item 002</li>';
-                        echo '<li class="list-group-item">item 003</li>';
-                    echo '</ul>';
-                    echo '<div class="card-block">';
-                        echo '<a href="https://www.youtube.com/watch?v=Kdaoe4hbMso" class="card-link">trailer</a>';
-                        echo '<a href="#" class="card-link">un autre link</a>';
+                      echo '<a href="#" class="card-link">'.$row['postTitle'].'</a>';
+                      echo '<a href="#" class="card-link">'.$row['postID'].'</a>';
                     echo '</div>';
                     echo '<div class="card-footer text-muted">this has been perma-featured</div>';
-                echo '</div>';
-            echo '</div>';
+                    echo '</div>';
+                }
+              } catch(PDOException $e) {
+                  echo $e->getMessage();
+              }
             ?>
-            <div class="col-xs-5">
-                <div class="card shutt_card">
-                    <div class="card-header">perma-featured</div>
-                    <img class="card-img-top" src="https://ubistatic19-a.akamaihd.net/ubicomstatic/en-us/global/search-thumbnail/fc5-wideart-table-search_thumnail_290060.jpg"
-                        alt="FC5 Official Site" />
-                    <div class="card-block">
-                        <h3 class="card-title">far cry 5 official site</h3>
-                        <p class="card-text">check it out to follow updates, etc.</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">item 001</li>
-                        <li class="list-group-item">item 002</li>
-                        <li class="list-group-item">item 003</li>
-                    </ul>
-                    <div class="card-block">
-                        <a href="https://far-cry.ubisoft.com/game/en-ca/home/" class="card-link">site</a>
-                        <a href="#" class="card-link">un autre link</a>
-                    </div>
-                    <div class="card-footer text-muted">this has been perma-featured</div>
-                </div>
+
             </div>
         </div>
     </div>

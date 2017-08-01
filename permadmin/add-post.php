bf9f08961e698b55ceced0a5278780319afba0ab
permafrown <bjoelh@gmail.com>
2017-08-01 16:33:18 -0400

    go

--- permadmin/add-post.php
+++ permadmin/add-post.php
@@ -122,6 +122,12 @@ if(empty($_SESSION['ulogin']))
         <p><label>Category</label><br />
 		<input type='text' name='postCat' value='<?php echo $_POST['postCat'];?>'></p>
 
+        <p><label>cat</label><br />
+        <select name="postCat">
+            <?php foreach($postCats as $postcat): ?>
+                <option value="<?php echo $postcat['postCat'] ?>"><?php echo $_POST['$postCat'] ?></<option>
+        </select>
+
 		<p><label>Brief Description | 300 words</label><br />
 		<textarea name='postDesc' cols='60' rows='10'><?php if(isset($error)){ echo $_POST['postDesc'];}?></textarea></p>
 

05763815ddb66465df2f527678dc839402bec4d6
joel <bjoelh@gmail.com>
2017-08-01 20:45:48 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -7,7 +7,9 @@
             echo '<div class="col">';
                 echo '<div class="card">';
                 echo '<div class="card-header">perma-featured</div>';
+                if (!empty('postImg')) {
                 echo '<img class="card-img-top" src="'.$row['postImg'].'" alt="'.$row['postTitle'].' image" />';
+              } else {echo <div></div>};
                   echo '<div class="card-block">';
                     echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
                     echo '<p class="card-text">'.$row['postDesc'].'</p>';
@@ -16,7 +18,7 @@
                       echo '<div class="card-block">';
                         echo '<a href="'.$row['postLink'].'" class="card-link" target="_blank">'.$row['postLinkText'].'</a>';
                       echo '</div>';
-                      }
+                    } else {echo <div></div>};
                     echo '<div class="card-footer text-muted">this has been perma-featured</div>';
                 echo '</div>';
             echo '</div>';

02ccf1eb77f01c77afa8f8ad68e2a99b1cc8375f
joel <bjoelh@gmail.com>
2017-08-01 20:24:36 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -2,12 +2,12 @@
     <?php
       try {
 
-        $stmt = $connect->query('SELECT postID, postTitle, postImg, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
+        $stmt = $connect->query('SELECT postID, postTitle, postImg, postLing, postLinkText, postFeat, postCat, postDesc, postDate FROM shutt_posts ORDER BY postID DESC');
         while($row = $stmt->fetch()){
             echo '<div class="col">';
                 echo '<div class="card">';
                 echo '<div class="card-header">perma-featured</div>';
-                echo '<img class="card-img-top" src="" alt="'.$row['postTitle'].' image" />';
+                echo '<img class="card-img-top" src="'.$row['postImg'].'" alt="'.$row['postTitle'].' image" />';
                   echo '<div class="card-block">';
                     echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
                     echo '<p class="card-text">'.$row['postDesc'].'</p>';

e5f1b04962f14df5aded7e544cd3f5346507407f
joel <bjoelh@gmail.com>
2017-08-01 20:33:14 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -12,11 +12,13 @@
                     echo '<h3 class="card-title"><a href="viewpost.php?id='.$row['postID'].'">'.$row['postTitle'].'</a></h3>';
                     echo '<p class="card-text">'.$row['postDesc'].'</p>';
                   echo '</div>';
-            echo '<div class="card-block">';
-              echo '<a href="'.$row['postLink'].'" class="card-link" target="_blank">'.$row['postLinkText'].'</a>';
-            echo '</div>';
-            echo '<div class="card-footer text-muted">this has been perma-featured</div>';
-            echo '</div>';
+                    if (!empty($postLink)) {
+                      echo '<div class="card-block">';
+                        echo '<a href="'.$row['postLink'].'" class="card-link" target="_blank">'.$row['postLinkText'].'</a>';
+                      echo '</div>';
+                      }
+                    echo '<div class="card-footer text-muted">this has been perma-featured</div>';
+                echo '</div>';
             echo '</div>';
         }
       } catch(PDOException $e) {

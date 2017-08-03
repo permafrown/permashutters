10bf65bb9d145fa3f1d3391bdc20485b0b2ea0f2
permafrown <bjoelh@gmail.com>
2017-08-03 15:48:33 -0400

    go

--- permadmin/add-post.php
+++ permadmin/add-post.php
@@ -63,7 +63,10 @@ if(empty($_SESSION['ulogin']))
 		if($postCont ==''){
 			$error[] = 'Please enter the content.';
 		}
-
+        if(isset('postFeat')) {
+            $_POST('postFeat') = 1;
+        } else {$_POST('postFeat') = 0;}
+        
 		if(!isset($error)){
 
 			try {

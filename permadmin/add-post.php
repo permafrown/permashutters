d4bd5a1d3a28f203389c7bb0b4566ea707e6593c
permafrown <bjoelh@gmail.com>
2017-08-03 15:54:23 -0400

    go

--- permadmin/add-post.php
+++ permadmin/add-post.php
@@ -120,7 +120,7 @@ if(empty($_SESSION['ulogin']))
 		<input type='text' name='postLinkText' value='<?php if(!empty('postLink')) {echo $_POST['postLinkText'];};?>'></p>
 
     <p><label>Featured?</label><br />
-		<input type='checkbox' name='postFeat' value='1'></p>
+		<input type='checkbox' name='postFeat' value='? 1 : 0'></p>
 
     <p><label>Category</label><br />
 		<input type='text' name='postCat' value='<?php echo $_POST['postCat'];?>'></p>

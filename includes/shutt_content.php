adfd316727cb4cc6832b2fbbf9be6558f070543a
permafrown <bjoelh@gmail.com>
2017-08-08 16:44:37 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -1,7 +1,6 @@
 <div class="row d-inline-flex">
     <?php
-    $postCatSel = :postCatSel;
-    :postCatSel = games;
+    $postCatSel = games;
     try {
         $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate ' .
         'FROM shutt_posts ' .

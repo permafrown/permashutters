581816fd9473c472ae5e927d52d4fb7e3c1e6fd7
permafrown <bjoelh@gmail.com>
2017-08-08 16:04:02 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -2,7 +2,7 @@
     <?php
     $postCatSel = 'games';
     try {
-        $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate FROM shutt_posts WHERE postCat = ':postCatSel' ORDER BY postDate DESC');
+        $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate FROM shutt_posts WHERE postCat = :postCatSel ORDER BY postDate DESC');
         $stmt->bindParam(':postCatSel', 'games', PDO::PARAM_STR);
         $stmt->execute();
         while($row = $stmt->fetch()){

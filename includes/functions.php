0cfc31899966ed0bc0a35afcb32894b9a9a1cd1f
permafrown <bjoelh@gmail.com>
2017-08-08 14:21:54 -0400

    go

--- includes/functions.php
+++ includes/functions.php
@@ -3,5 +3,7 @@ function slug($str) {
     $str = str_ireplace(strtolower(trim($str)));
     $str = preg_replace('/[^\w\d\-\ ]/', '', $str);
     $str = str_replace(' ', '', $str);
-    return preg_replace('/\-{2,}/', '', $str);
+    $str = preg_replace('/\-{2,}/', '', $str);
 }
+return $str;
+?>

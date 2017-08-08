9e185ec4235adac11f413642590ca565cef97b4c
permafrown <bjoelh@gmail.com>
2017-08-08 11:08:29 -0400

    testing post functionality

--- permadmin/tester.php
+++ permadmin/tester.php
@@ -1,22 +1,26 @@
+<?php print_r($_POST); ?>
+
 <html>
 <head>
     <title>tester input</title>
 </head>
 <body>
-    <p>
-        <input type="text" name="post_title" />
-    </p>
-    <p>
-        <textarea name="post_content"></textarea>
-    </p>
-    <p>
-        <label for="post_category">cat1</label>
-        <input type="checkbox" name="post_category" value="cat1" />
-        <label for="post_category">cat2</label>
-        <input type="checkbox" name="post_category" value="cat2" />
-    </p>
-    <p>
-        <input type="submit" value="submit" />
-    </p>
+    <form method="post">
+        <p>
+            <input type="text" name="post_title" />
+        </p>
+        <p>
+            <textarea name="post_content"></textarea>
+        </p>
+        <p>
+            <label for="post_category">cat1</label>
+            <input type="checkbox" name="post_category" value="cat1" />
+            <label for="post_category">cat2</label>
+            <input type="checkbox" name="post_category" value="cat2" />
+        </p>
+        <p>
+            <input type="submit" value="submit" />
+        </p>
+    </form>
 </body>
 </html>

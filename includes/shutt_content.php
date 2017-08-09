11fbd43924600e33724d1fc00b0848a4878b3313
permafrown <bjoelh@gmail.com>
2017-08-08 20:37:17 -0400

    go

--- includes/shutt_content.php
+++ includes/shutt_content.php
@@ -4,9 +4,9 @@
     try {
         $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate ' .
         'FROM shutt_posts ' .
-        // 'WHERE postCat = :postCatSel ' .
+        'WHERE postCat < :postCatSel ' .
         'ORDER BY postDate DESC');
-        // $stmt->bindValue(':postCatSel', $postCatSel, PDO::PARAM_STR);
+        $stmt->bindParam(':postCatSel', $postCatSel, PDO::PARAM_STR);
         while($row = $stmt->fetch()){
             echo '<div class="col">';
                 echo '<div class="card">';

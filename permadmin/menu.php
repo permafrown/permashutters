87ba816f29e628f6f4904451639ff2d94494b00e
permafrown <bjoelh@gmail.com>
2017-08-08 20:32:06 -0400

    go

--- permadmin/menu.php
+++ permadmin/menu.php
@@ -1,8 +1,6 @@
 <h2>admin menu</h2>
 <ul class='admin_nav_menu'>
 	<li><a href='index.php'>menu</a></li>
-	<!--<li><a href='users.php'>users</a></li>-->
-    <li><a href='categories.php'>cats</a></li>
 	<li><a href="../" target="_blank">site</a></li>
 	<li><a href='auth/logout.php'>logout</a></li>
 </ul>

a1a932dbaf29dcaf3e9fb7b486083575ee278715
permafrown <bjoelh@gmail.com>
2017-08-10 14:46:59 -0400

    go

--- includes/shutter_menu.php
+++ includes/shutter_menu.php
@@ -1,8 +1,8 @@
 <div class="column shutters">
-    <a class="shutt col-xs-2 games_shutt" href="games.php"><span class="hidden-sm hidden-xs">games</span></a>
-    <a class="shutt col-xs-2 fauna_shutt" href="fauna.php"><span class="hidden-sm hidden-xs">fauna</span></a>
-    <a class="shutt col-xs-2 science_shutt" href="science.php"><span class="hidden-sm hidden-xs">science</span></a>
-    <a class="shutt col-xs-2 words_shutt" href="words.php"><span class="hidden-sm hidden-xs">words</span></a>
-    <a class="shutt col-xs-2 sundry_shutt" href="sundry.php"><span class="hidden-sm hidden-xs">sundry</span></a>
-    <a class="shutt col-xs-2 media_shutt" href="media.php"><span class="hidden-sm hidden-xs">media</span></a>
+    <a class="shutt col-xs-2 games_shutt" href="content.php?cat=games"><span class="hidden-sm hidden-xs">games</span></a>
+    <a class="shutt col-xs-2 fauna_shutt" href="content.php?cat=fauna"><span class="hidden-sm hidden-xs">fauna</span></a>
+    <a class="shutt col-xs-2 science_shutt" href="content.php?cat=science"><span class="hidden-sm hidden-xs">science</span></a>
+    <a class="shutt col-xs-2 words_shutt" href="content.php?cat=words"><span class="hidden-sm hidden-xs">words</span></a>
+    <a class="shutt col-xs-2 sundry_shutt" href="content.php?cat=sundry"><span class="hidden-sm hidden-xs">sundry</span></a>
+    <a class="shutt col-xs-2 media_shutt" href="content.php?cat=media"><span class="hidden-sm hidden-xs">media</span></a>
 </div>

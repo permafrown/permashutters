6dcff0b00a6553fe4812786678dd1cf096b72aa3
permafrown <bjoelh@gmail.com>
2017-08-10 14:10:31 -0400

    go

--- fauna.php
+++ fauna.php
@@ -21,7 +21,7 @@
           <?php
           $postCatSel = "games";
           try {
-              $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate FROM shutt_posts WHERE postCat = '$postCatSel' ORDER BY postDate DESC');
+              $stmt = $connect->query('SELECT postID, postTitle, postSlug, postImg, postLink, postLinkText, postFeat, postCat, postDesc, postDate FROM shutt_posts WHERE postCat = :postCatSel ORDER BY postDate DESC');
               // $stmt->bindParam(':postCatSel', $postCatSel, PDO::PARAM_STR);
               while($row = $stmt->fetch()){
                   echo '<div class="col">';

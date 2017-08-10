3f5427d66193df6c5b3e57789544c87f1a43a93e
permafrown <bjoelh@gmail.com>
2017-08-10 16:09:18 -0400

    go

--- content.php
+++ content.php
@@ -6,11 +6,11 @@
     <?php include_once 'includes/head.php';?>
     <?php include_once 'permadmin/auth/config.php'; ?>
     <?php $postCatSel = $_GET['cat']; ?>
-    <title><?php echo $postCatSel ?> | permashutters</title>
+    <title><?php echo $postCatSel; ?> | permashutters</title>
 </head>
 
 <body>
-    <h1 class="title_centered" style="margin-top: 10%; color: #0F0;"><small>permashutters</small><?php echo $postCatSel ?></h1>
+    <h1 class="title_centered" style="margin-top: 10%; color: #0F0;"><small>permashutters</small><?php echo $postCatSel; ?></h1>
     <div class="container-fluid">
         <div class="col-xs-12">
             <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
@@ -20,7 +20,7 @@
     <div class="container-fluid shutt_page_content">
         <?php
             if (empty($postCatSel) OR $postCatSel = "all") {
-                include 'includes/shutt_content.php';
+                include 'includes/all_content.php';
             } elseif (!empty($postCatSel)) {
                 include 'includes/shutt_content.php';
             } else {echo "<div>select a category, bruh</div>";

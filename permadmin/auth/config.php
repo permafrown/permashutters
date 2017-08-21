90fb210be04707ba149156bd4aef737b6933e176
permafrown <bjoelh@gmail.com>
2017-08-21 16:53:31 -0400

    go

--- permadmin/auth/config.php
+++ permadmin/auth/config.php
@@ -4,14 +4,14 @@ session_start();
 require_once("cred.php");
 
 // Define database
-define('dbhost', $DB_HOST);
-define('dbuser', $DB_UW);
-define('dbpass', $DB_PW);
-define('dbname', $DB_NAME);
+// define('dbhost', $DB_HOST);
+// define('dbuser', $DB_UN);
+// define('dbpass', $DB_PW);
+// define('dbname', $DB_NAME);
 
 // Connecting database
 try {
-	$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname.", dbuser, dbpass");
+	$connect = new PDO("mysql:host=$DB_HOST; dbname=$DB_NAME, $DB_UN, $DB_PW");
 	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {

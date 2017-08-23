66e4708bf07f20d75c921c58c694437d2330b518
permafrown <bjoelh@gmail.com>
2017-08-23 16:28:18 -0400

    go

--- index.php
+++ index.php
@@ -13,10 +13,6 @@
   <h1 class="title_centered" style="margin-top: 2.5%; color: #0F0;">permashutters</h1>
   <!-- END MAIN TITLE -->
 
-  <!-- MED SCREEN SUBMENU -->
-  <h2 class="submenu hidden-sm-down"><a href="content.php?cat=all">all</a> | <a href="content.php?cat=games">games</a> | <a href="content.php?cat=fauna">fauna</a> | <a href="content.php?cat=science">science</a> | <a href="content.php?cat=words">words</a>    | <a href="content.php?cat=sundry">sundry</a> | <a href="content.php?cat=media">media</a> | <a href="about.php">about</a></h2>
-  <!-- END MED SCREEN SUBMENU -->
-
   <!-- SHUTTER MENU -->
   <div class="container-fluid hidden-sm-down">
     <div class="col-xs-12">
@@ -39,6 +35,10 @@
   <!-- SMALL SCREEN SUBMENU -->
   <h2 class="submenu hidden-md-up"><a href="about.php">about</a></h2>
   <!-- END SMALL SCREEN SUBMENU -->
+
+  <!-- MED SCREEN SUBMENU -->
+  <h2 class="submenu hidden-sm-down"><a href="content.php?cat=all">all</a> | <a href="content.php?cat=games">games</a> | <a href="content.php?cat=fauna">fauna</a> | <a href="content.php?cat=science">science</a> | <a href="content.php?cat=words">words</a>    | <a href="content.php?cat=sundry">sundry</a> | <a href="content.php?cat=media">media</a> | <a href="about.php">about</a></h2>
+  <!-- END MED SCREEN SUBMENU -->
 
   <!-- SCRIPTS -->
   <script src="js/jquery-3.1.1.min.js"></script>

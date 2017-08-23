9820416043f2c253e602570ab1059edd623be8c6
permafrown <bjoelh@gmail.com>
2017-08-23 16:54:06 -0400

    shifting shutters to horizontal on small screen

--- index.php
+++ index.php
@@ -22,18 +22,12 @@
   <h2 class="submenu subtext hidden-sm-down">a collection of things i find interesting</h2>
   <hr class="perma_hr">
   <div class="column shutters">
-    <!-- <a class="shutt col-xs-2 games_shutt" href="content.php?cat=games"><span class="hidden-sm-down">games</span></a>
+    <a class="shutt col-xs-2 games_shutt" href="content.php?cat=games"><span class="hidden-sm-down">games</span></a>
     <a class="shutt col-xs-2 fauna_shutt" href="content.php?cat=fauna"><span class="hidden-sm-down">fauna</span></a>
     <a class="shutt col-xs-2 science_shutt" href="content.php?cat=science"><span class="hidden-sm-down">science</span></a>
     <a class="shutt col-xs-2 words_shutt" href="content.php?cat=words"><span class="hidden-sm-down">words</span></a>
     <a class="shutt col-xs-2 sundry_shutt" href="content.php?cat=sundry"><span class="hidden-sm-down">sundry</span></a>
-    <a class="shutt col-xs-2 media_shutt" href="content.php?cat=media"><span class="hidden-sm-down">media</span></a> -->
-    <a class="shutt col-xs-2 games_shutt" href="content.php?cat=games">games</a>
-    <a class="shutt col-xs-2 fauna_shutt" href="content.php?cat=fauna">fauna</a>
-    <a class="shutt col-xs-2 science_shutt" href="content.php?cat=science">science</a>
-    <a class="shutt col-xs-2 words_shutt" href="content.php?cat=words">words</a>
-    <a class="shutt col-xs-2 sundry_shutt" href="content.php?cat=sundry">sundry</a>
-    <a class="shutt col-xs-2 media_shutt" href="content.php?cat=media">media</a>
+    <a class="shutt col-xs-2 media_shutt" href="content.php?cat=media"><span class="hidden-sm-down">media</span></a>
   </div>
   <hr class="perma_hr">
   <!-- END SHUTTER MENU -->

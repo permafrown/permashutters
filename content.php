963612e9e06507eabf418627b74a355ad5afa046
permafrown <bjoelh@gmail.com>
2017-08-24 13:31:42 -0400

    go

--- content.php
+++ content.php
@@ -27,11 +27,6 @@
             }
         ?>
     </h1>
-    <div class="container-fluid">
-        <div class="col-xs-12">
-            <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
-        </div>
-    </div>
     <hr class="perma_hr">
     <div class="container-fluid shutt_page_content">
         <?php
@@ -53,6 +48,13 @@
     <hr class="perma_hr">
 
     <?php include_once 'includes/shutter_menu.php';?>
+
+    <div class="container-fluid">
+        <div class="col-xs-12">
+            <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
+        </div>
+    </div>
+
     <?php include_once 'includes/body_scripts.php';?>
 </body>
 

0df3830033bd3afbf3fb05b6c05ca5172ffb3b6c
permafrown <bjoelh@gmail.com>
2017-08-29 15:37:20 -0400

    go

--- content.php
+++ content.php
@@ -18,7 +18,7 @@
 </head>
 
 <body>
-    <h1 class="title_centered" style="margin-top: 10%; color: #0F0;">
+    <h1 class="title_centered" style="color: #0F0;">
         <?php
             if (($postCatSel === "") OR ($postCatSel === "all")) {
                 echo "<small>permashutters</small>";
@@ -27,11 +27,6 @@
             }
         ?>
     </h1>
-    <div class="container-fluid">
-        <div class="col-xs-12">
-            <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
-        </div>
-    </div>
     <hr class="perma_hr">
     <div class="container-fluid shutt_page_content">
         <?php
@@ -53,6 +48,13 @@
     <hr class="perma_hr">
 
     <?php include_once 'includes/shutter_menu.php';?>
+
+    <div class="container-fluid">
+        <div class="col-xs-12">
+            <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="https://dl.dropboxusercontent.com/u/5650853/permafrown_avatar_kingdom_come_spectre.png" alt="avatar" /></a>
+        </div>
+    </div>
+
     <?php include_once 'includes/body_scripts.php';?>
 </body>
 

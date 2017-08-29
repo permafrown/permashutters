d9c397d7d9626e93d06e1e9b1fadee35a306d112
permafrown <bjoelh@gmail.com>
2017-08-29 15:31:10 -0400

    go

--- content.php
+++ content.php
@@ -18,7 +18,7 @@
 </head>
 
 <body>
-    <h1 class="title_centered" style="margin-top: 10%;">
+    <h1 class="title_centered">
         <?php
             if (($postCatSel === "") OR ($postCatSel === "all")) {
                 echo "<small>permashutters</small>";

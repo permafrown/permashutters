83b435801a66b7bcef93ca7d1cff618e4616e260
permafrown <bjoelh@gmail.com>
2017-09-01 16:21:48 -0400

    go

--- gerbil.php
+++ gerbil.php
@@ -1,1 +1,25 @@
-gerbils are neat
+<!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
+<?php
+echo 'gerbils are neat';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <?php include_once 'includes/head.php';?>
+  <?php include_once 'permadmin/auth/config.php'; ?>
+  <title>gerbil | permashutters</title>
+</head>
+<body>
+
+  <hr class="permahr">
+  <div class="container-fluid">
+      <div class="col-xs-12">
+          <a href="index.php"><img style="margin-bottom: 5%;" class="img-responsive avatar center-block title-centered" src="http://bit.ly/permavatar" alt="avatar" /></a>
+      </div>
+  </div>
+  <hr class="permahr">
+
+  <?php include_once "/includes/body_scripts.php"; ?>
+</body>
+</html>

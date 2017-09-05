05fe2e4460921ebe2d6743cfa33b6c040f1b5300
permafrown <bjoelh@gmail.com>
2017-09-05 15:04:43 -0400

    go

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   <!-- AVATAR -->
   <div class="container-fluid hidden-sm-down">
     <div class="col-xs-12">
-      <a href="index.php"><img class="img-responsive avatar center-block title-centered" src="http://bit.ly/permavatar" alt="avatar" /></a>
+      <a href="index.php"><img class="img-responsive avatar center-block title-centered" src="img/permavatar.png" alt="avatar" /></a>
     </div>
   </div>
   <!-- END AVATAR -->

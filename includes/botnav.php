2050e51b883a17dcc1c2a118f66e67bfbea6267d
permafrown <bjoelh@gmail.com>
2017-09-05 15:00:19 -0400

    go

--- includes/botnav.php
+++ includes/botnav.php
@@ -1,5 +1,5 @@
 <div class="container bot_nav">
-  <div class="row">
+  <div class="row bot_row">
     <div class="col-xs-12 foot_nav">
       <button class="btn"><a href="https://www.facebook.com/">fb</a></button>
       <button class="btn"><a href="https://twitter.com/">tw</a></button>

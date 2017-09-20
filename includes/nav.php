1513ea88f7d07a50d64542760c29162422fffc8f
permafrown <bjoelh@gmail.com>
2017-09-20 14:37:44 -0400

    adding nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <nav class="navbar navbar-expand-md navbar-dark bg-light">
+  <nav class="navbar fixed-bottom navbar-expand-md navbar-dark" style="background-color: #BBB;">
     <!-- <a class="navbar-brand "href="#">perma<i class="fa fa-circle-o-notch" aria-hidden="true"></i>frown</a> -->
     <a class="navbar-brand "href="./index.php">perma<img src="./img/permavatar.png" width="30" height="30" class="d-inline-block align-top" alt="permavatar">shutters</i></a>
     <!-- <a class="navbar-brand "href="./index.php">perma<i class="fa fa-ravelry" aria-hidden="true">shutters</i></a> -->
@@ -8,22 +8,22 @@
     </button>
 
     <div class="collapse navbar-collapse" id="hamburg">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item"><a class="nav-link" href="./index.php">home</a></li>
-        <li class="nav-item"><a class="nav-link" href="./about.php">about</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=feat">featured</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=all">all</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=games">games</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=fauna">fauna</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=science">science</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=words">words</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=sundry">sundry</a></li>
-        <li class="nav-item"><a class="nav-link" href="./content.php?cat=media">media</a></li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item"><a class="nav-link" href="#">sign up <i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
-        <li class="nav-item"><a class="nav-link" href="./permadmin/index.php">login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
-      </ul>
+      <div class="navbar-nav mr-auto">
+        <a class="nav-item nav-link" href="./index.php">home</a>
+        <a class="nav-item nav-link" href="./about.php">about</a>
+        <a class="nav-item nav-link" href="./content.php?cat=feat">featured</a>
+        <a class="nav-item nav-link" href="./content.php?cat=all">all</a>
+        <a class="nav-item nav-link" href="./content.php?cat=games">games</a>
+        <a class="nav-item nav-link" href="./content.php?cat=fauna">fauna</a>
+        <a class="nav-item nav-link" href="./content.php?cat=science">science</a>
+        <a class="nav-item nav-link" href="./content.php?cat=words">words</a>
+        <a class="nav-item nav-link" href="./content.php?cat=sundry">sundry</a>
+        <a class="nav-item nav-link" href="./content.php?cat=media">media</a>
+      </div>
+      <div class="navbar-nav ml-auto">
+        <a class="nav-item nav-link" href="#">sign up <i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
+        <a class="nav-item nav-link" href="./permadmin/index.php">login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
+      </div>
     </div>
   </nav>
 </div>

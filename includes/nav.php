c66dba64a07a83b6ad77394a237e64c7e3fb8da8
permafrown <bjoelh@gmail.com>
2017-09-20 14:29:17 -0400

    adding nav

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
     <!-- <a class="navbar-brand "href="#">perma<i class="fa fa-circle-o-notch" aria-hidden="true"></i>frown</a> -->
     <a class="navbar-brand "href="./index.php">perma<i class="fa fa-ravelry" aria-hidden="true">shutters</i></a>
     <button class="navbar-toggler" type="button" name="button" data-toggle="collapse" data-target="#hamburg" aria-controls="hamburg" aria-expanded="false" aria-label="toggle nav">
@@ -8,13 +8,20 @@
 
     <div class="collapse navbar-collapse" id="hamburg">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active"><a class="nav-link" href="#">home <span class="sr-only">(current)</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="#">about</a></li>
-        <li class="nav-item"><a class="nav-link" href="#">contact</a></li>
+        <li class="nav-item"><a class="nav-link" href="./index.php">home</a></li>
+        <li class="nav-item"><a class="nav-link" href="./about.php">about</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=feat">featured</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=all">all</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=games">games</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=fauna">fauna</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=science">science</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=words">words</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=sundry">sundry</a></li>
+        <li class="nav-item"><a class="nav-link" href="./content.php?cat=media">media</a></li>
       </ul>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item"><a class="nav-link" href="#">sign up <i class="fa fa-user-plus" aria-hidden="true"></i></a></li>
-        <li class="nav-item"><a class="nav-link" href="#">login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
+        <li class="nav-item"><a class="nav-link" href="./permadmin/index.php">login <i class="fa fa-sign-in" aria-hidden="true"></i></a></li>
       </ul>
     </div>
   </nav>

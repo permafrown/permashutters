cbe07335d5ac8cdcdb17b4ca23e0628b6b7e80de
permafrown <bjoelh@gmail.com>
2017-10-03 14:12:18 -0400

    adding colour game

--- crit/colourGame.php
+++ crit/colourGame.php
@@ -19,6 +19,7 @@
       <h3><span id="colourDisplay"></span></h3>
         <div id="stripe">
           <button id="back" type="button" name="button"><a href="../../content.php?cat=games">Back</a></button>
+          <span> | </span>
           <button id="reset" type="button" name="button">New Colours?</button>
           <span id="message"></span>
           <button class="mode" type="button" name="button">Easy</button>

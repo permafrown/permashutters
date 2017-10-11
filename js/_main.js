5459966433c0223212791971d3bb02a61272f620
permafrown <bjoelh@gmail.com>
2017-10-11 14:36:28 -0400

    fixes

--- js/_main.js
+++ js/_main.js
@@ -7,9 +7,9 @@ function hamburg() {
     }
 }
 
-$('.games_shutt').css("background", "url(../img/games_bg.jpg) no-repeat");
-$('.fauna_shutt').css("background", "url(../img/fauna_bg_04.jpg) no-repeat");
-$('.science_shutt').css("background", "url(../img/science_bg.jpg) no-repeat");
-$('.words_shutt').css("background", "url(../img/words_bg.jpg) no-repeat");
-$('.sundry_shutt').css("background", "url(../img/sundry_bg.jpg) no-repeat");
-$('.media_shutt').css("background", "url(../img/media_bg_02.jpg) no-repeat");
+// $('.games_shutt').css("background", "url(../img/games_bg.jpg) no-repeat");
+// $('.fauna_shutt').css("background", "url(../img/fauna_bg_04.jpg) no-repeat");
+// $('.science_shutt').css("background", "url(../img/science_bg.jpg) no-repeat");
+// $('.words_shutt').css("background", "url(../img/words_bg.jpg) no-repeat");
+// $('.sundry_shutt').css("background", "url(../img/sundry_bg.jpg) no-repeat");
+// $('.media_shutt').css("background", "url(../img/media_bg_02.jpg) no-repeat");

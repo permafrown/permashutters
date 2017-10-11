0a278d0df9c923987ef5c63eb010a90ae0dd092c
permafrown <bjoelh@gmail.com>
2017-10-11 13:34:39 -0400

    go

--- crit/playGames.php
+++ crit/playGames.php
@@ -3,23 +3,23 @@
 <html lang="en">
 
 <head>
-    <?php include_once '../../includes/head.php';?>
-    <?php include_once '../../permadmin/auth/config.php'; ?>
+    <?php include_once '../includes/head.php';?>
+    <?php include_once '../permadmin/auth/config.php'; ?>
     <title>permaGames</title>
 </head>
 
 <body>
     <h1 class="title_centered" style="color: #0F0;">perma<i class="fa fa-ravelry" aria-hidden="true"></i>games</h1>
-    <p><a href="../../index.php">home</a> | <a href="../../about.php">about</a> | <a href="../../content.php?cat=games">games</a></p>
+    <p><a href="../index.php">home</a> | <a href="../about.php">about</a> | <a href="../content.php?cat=games">games</a></p>
     <hr class="perma_hr">
     <p><a href="colourGame.php">colour game</a> | <a href="circles.php">circles</a></p>
     <hr class="perma_hr">
 
-    <?php include_once '../../includes/shutter_menu.php';?>
+    <?php include_once '../includes/shutter_menu.php';?>
 
-    <?php include_once '../../includes/avatar.php';?>
+    <?php include_once '../includes/avatar.php';?>
 
-    <?php include_once '../../includes/body_scripts.php';?>
+    <?php include_once '../includes/body_scripts.php';?>
 </body>
 
 </html>

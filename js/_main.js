e69f0ff96117701caec2c87ead6637cca705e7ef
permafrown <bjoelh@gmail.com>
2017-10-11 14:24:45 -0400

    fixes

--- js/_main.js
+++ js/_main.js
@@ -6,3 +6,46 @@ function hamburg() {
         x.className = "topnav";
     }
 }
+
+$('.games_shutt').css("background-image", "../img/games_bg.jpg");
+
+
+// .fauna_shutt {
+//     background-image: url("../img/fauna_bg_04.jpg");
+//     transition: 0.5s ease;
+//     -webkit-transition: 0.5s ease;
+//     -moz-transition: 0.5s ease;
+//     -o-transition: 0.5s ease;
+// }
+//
+// .science_shutt {
+//     background-image: url("../img/science_bg.jpg");
+//     transition: 0.5s ease;
+//     -webkit-transition: 0.5s ease;
+//     -moz-transition: 0.5s ease;
+//     -o-transition: 0.5s ease;
+// }
+//
+// .words_shutt {
+//     background-image: url("../img/words_bg.jpg");
+//     transition: 0.5s ease;
+//     -webkit-transition: 0.5s ease;
+//     -moz-transition: 0.5s ease;
+//     -o-transition: 0.5s ease;
+// }
+//
+// .sundry_shutt {
+//     background-image: url("../img/sundry_bg.jpg");
+//     transition: 0.5s ease;
+//     -webkit-transition: 0.5s ease;
+//     -moz-transition: 0.5s ease;
+//     -o-transition: 0.5s ease;
+// }
+//
+// .media_shutt {
+//     background-image: url("../img/media_bg_02.jpg");
+//     transition: 0.5s ease;
+//     -webkit-transition: 0.5s ease;
+//     -moz-transition: 0.5s ease;
+//     -o-transition: 0.5s ease;
+// }

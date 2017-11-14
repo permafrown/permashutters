907010f622d8ae5e937d9c6d5e6f35dd70b87432
permafrown <bjoelh@gmail.com>
2017-11-14 14:40:00 -0500

    adding weather app

---
+++ crit/weather.php
@@ -0,0 +1,22 @@
+<!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <?php include_once '../includes/head.php';?>
+    <?php include_once '../permadmin/auth/config.php'; ?>
+  <title>gerbil | permashutters</title>
+</head>
+<body>
+  <h1 class="title-centered">gerbil | permashutters</h1>
+  <hr class="permahr">
+  <div class="container-fluid">
+      <div class="col-xs-12">
+          <a href="index.php"><img class="img-responsive avatar center-block title-centered" src="http://bit.ly/permavatar" alt="avatar" /></a>
+      </div>
+  </div>
+  <hr class="permahr">
+
+    <?php include_once "includes/body_scripts.php"; ?>
+</body>
+</html>

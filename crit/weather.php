9c3bd19c7e2e62e6063dc28a30bbaa506245f83a
permafrown <bjoelh@gmail.com>
2017-11-14 15:12:25 -0500

    updating weather app

--- crit/weather.php
+++ crit/weather.php
@@ -1,9 +1,16 @@
 <!-- Copyright 2016. GoodLife Music Ltd.. All Rights Reserved. -->
+<?php
+if ($_GET['cityName']) {
+    $forecastPage = fire_get_contents("https://www.weather-forecast.com/locations/Toronto/forecasts/latest");
+    echo $forecastPage;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <html>
 <head>
-  <?php include_once '../includes/head.php';?>
+    <?php include_once '../includes/head.php';?>
     <?php include_once '../permadmin/auth/config.php'; ?>
     <link rel="stylesheet" type="text/css" href="css/weather.css" />
   <title>weather | permashutters</title>
@@ -13,12 +20,12 @@
   <hr class="permahr">
   <div class="container">
     <h2 class="title-centered">what's the weather?</h2>
-    <form action="#" method="POST">
+    <form action="#" method="GET">
       <div class="form-group">
         <label for="cityName">enter a city</label>
         <input type="text" class="form-control" id="cityName" name="cityName" placeholder="city"></input>
       </div>
-      <button type="submit" class="btn btn-primary" name="submit">check</button>
+      <button type="submit" class="btn btn-primary">check</button>
     </form>
     <div class="jumbotron">
       <p>content</p>

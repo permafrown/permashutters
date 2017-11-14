c4d439cc850e0691c47e93ea89d1d3647c64f90f
permafrown <bjoelh@gmail.com>
2017-11-14 14:48:00 -0500

    updating weather app

--- crit/weather.php
+++ crit/weather.php
@@ -10,6 +10,14 @@
 <body>
   <h1 class="title-centered">weather | permashutters</h1>
   <hr class="permahr">
+  <div class="container">
+    <h2 class="title-centered">what's the weather?</h2>
+    <p>enter your city name</p>
+    <form action="#" method="POST">
+      <input type="text" name="cityName" placeholder="city"></input>
+      <input type="submit" name="submit">check</input>
+    </form>
+  </div>
 
   <hr class="permahr">
     <?php include_once "includes/body_scripts.php"; ?>

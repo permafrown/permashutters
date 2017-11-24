71eb897830bfdaa1d7f921354fc82e4d858b68fd
permafrown <bjoelh@gmail.com>
2017-11-24 12:24:39 -0500

    update weather app to google maps javascript api

--- crit/weather.php
+++ crit/weather.php
@@ -65,26 +65,9 @@ if ($_GET['cityName']) {
             }
             ?>
         </div>
-        <!-- <div class="jumbotron"> -->
-        <div id="map"></div>
-            <script>
-              var map;
-              function initMap() {
-                map = new google.maps.Map(document.getElementById('map'), {
-                  center: {lat: -34.397, lng: 150.644},
-                  zoom: 8
-                });
-              }
-            </script>
-            <?php
-            // if ($weatherArray != "") {
-            //     echo "<iframe width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyDbR0TOMMHuzQFYlIAZsEA1TaqgYG526k4&q=".$weatherArray['name']."+".$weatherArray['sys']['country']."\" allowfullscreen></iframe>";
-            // } else {
-            //     echo "";
-            // }
-             ?>
-
-        <!-- </div> -->
+        <div class="jumbotron">
+            <div id="map"></div>
+        </div>
       </div>
     </div>
   </div>

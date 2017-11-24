a3e2beb98022379fb512ba6ab6a0f7be93d46aa1
permafrown <bjoelh@gmail.com>
2017-11-24 09:28:40 -0500

    update weather app to use OWM API

--- crit/weather.php
+++ crit/weather.php
@@ -67,7 +67,7 @@ if ($_GET['cityName']) {
         <div class="jumbotron">
             <?php
             if ($weatherArray != "") {
-                echo "<iframe width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyDbR0TOMMHuzQFYlIAZsEA1TaqgYG526k4&q=Space+Needle,Seattle+WA\" allowfullscreen></iframe>";
+                echo "<iframe width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" src=\"https://www.google.com/maps/embed/v1/place?key=AIzaSyDbR0TOMMHuzQFYlIAZsEA1TaqgYG526k4&q=".$weatherArray['name']."+".$weatherArray['sys']['country']."\" allowfullscreen></iframe>";
             } else {
                 echo "";
             }

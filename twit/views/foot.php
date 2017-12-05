dc9c58f5827fa70094423c15d250d556a3e2b473
permafrown <bjoelh@gmail.com>
2017-12-05 14:11:35 -0500

    adding permaTwit

--- twit/views/foot.php
+++ twit/views/foot.php
@@ -15,6 +15,7 @@
       </div>
       <div class="modal-body">
           <form>
+            <input type="hidden" name="loginActive" value="1">
             <div class="form-group">
               <label for="email">Email Address</label>
               <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
@@ -28,13 +29,19 @@
           </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-outline-dark" data-dismiss="modal">Close</button>
+          <a class="btn btn-outline-dark" id="toggleLogin">Sign Up?</a>
+        <button type="button" class="btn btn-outline-dark mr-auto" data-dismiss="modal">Close</button>
         <button type="button" class="btn btn-dark">Login</button>
       </div>
     </div>
   </div>
 </div>
+        <script>
+            $("#toggleLogin").click(function() {
+                console.log('clicked it');
+            });
 
+        </script>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>

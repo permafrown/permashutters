cea25a351e313979f5599029af5b194d1243c019
permafrown <bjoelh@gmail.com>
2017-12-05 14:58:23 -0500

    adding permaTwit

--- twit/actions.php
+++ twit/actions.php
@@ -3,6 +3,21 @@
     include("functions.php");
 
     if ($_GET['action'] == 'loginSignup') {
+
+        $error = "";
+
+        if (!$_POST['email']) {
+            $error = "email required...";
+        } else if (!$_POST['password']) {
+            $error = "password required...";
+        } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+            $error = "Not a valid email address...";
+        }
+
+        if ($error != "") {
+            echo $error;
+        }
+
         if ($_POST['loginActive'] == "0");
             echo "Sign user up";
     }

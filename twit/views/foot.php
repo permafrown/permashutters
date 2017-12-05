5a32519324c20601e44c58d32607d51eba041667
permafrown <bjoelh@gmail.com>
2017-12-05 14:13:21 -0500

    adding permaTwit

--- twit/views/foot.php
+++ twit/views/foot.php
@@ -29,9 +29,9 @@
           </form>
       </div>
       <div class="modal-footer">
-          <a class="btn btn-outline-dark" id="toggleLogin">Sign Up?</a>
+          <a class="btn btn-outline-dark ml-auto" id="toggleLogin">Sign Up?</a>
         <button type="button" class="btn btn-outline-dark mr-auto" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-dark">Login</button>
+        <button type="button" class="btn btn-dark ml-auto">Login</button>
       </div>
     </div>
   </div>

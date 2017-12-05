626636f8c4ca7c619a71b7c3cf0373e45ccde905
permafrown <bjoelh@gmail.com>
2017-12-05 16:11:58 -0500

    adding permaTwit

--- twit/actions.php
+++ twit/actions.php
@@ -37,26 +37,31 @@
             $error = 'enter pw';
 
         if($error == ''){
-            $query = $connect->prepare("SELECT * FROM tw_users WHERE tw_un = ?");
-            $query->bindValue(1, $un);
-            $query->execute();
-
-            if($query->rowCount() > 0) {
-                $error = 'user exists...';
-                exit();
-            } else {
-                try {
-                    $stmt = $connect->prepare('INSERT INTO tw_users (tw_un, tw_pw) VALUES (:un, :pw)');
-                    $stmt->execute(array(
-                        ':un' => $un,
-                        ':pw' => $pw,
-                        ));
-                    header('Location: /index.php');
-                    exit();
-                }
-                catch(PDOException $e) {
-                    echo $e->getMessage();
-                }
+            if ($_POST['loginActive'] == "0") {
+                console.log("tried to sign up");
+                // $query = $connect->prepare("SELECT * FROM tw_users WHERE tw_un = ?");
+                // $query->bindValue(1, $un);
+                // $query->execute();
+                //
+                // if($query->rowCount() > 0) {
+                //     $error = 'user exists...';
+                //     exit();
+                // } else {
+                //     try {
+                //         $stmt = $connect->prepare('INSERT INTO tw_users (tw_un, tw_pw) VALUES (:un, :pw)');
+                //         $stmt->execute(array(
+                //             ':un' => $un,
+                //             ':pw' => $pw,
+                //             ));
+                //         header('Location: /index.php');
+                //         exit();
+                //     }
+                //     catch(PDOException $e) {
+                //         echo $e->getMessage();
+                //     }
+                // }
+            } else if ($_POST['loginActive'] == "1") {
+                console.log("tried to log in");
             }
         } else {
             echo $error;

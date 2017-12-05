b889083db543f429618cf5e8c9996272d0ace3d0
permafrown <bjoelh@gmail.com>
2017-12-05 13:44:34 -0500

    adding permaTwit

--- twit/views/home.php
+++ twit/views/home.php
@@ -1,4 +1,7 @@
 
-<div class="container">
-    <h1>perma<i class="fa fa-twitter" aria-hidden="true"></i>Twit</h1>
-</div>
+<main role="main" class="container">
+    <div class="mt-3">
+        <h1>perma<i class="fa fa-twitter" aria-hidden="true"></i>Twit</h1>
+    </div>
+    <p class="lead">permafrown's twitter clone</p>
+</main>

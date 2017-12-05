94be9f7e433fef135268f497b5dbc9622d5b11b6
permafrown <bjoelh@gmail.com>
2017-12-05 16:57:48 -0500

    adding permaTwit

--- twit/actions.php
+++ twit/actions.php
@@ -3,25 +3,25 @@
     include("functions.php");
 
     if ($_GET['action'] == 'loginSignup') {
-
-        $error = "";
-
-        if (!$_POST['email']) {
-            $error = "email required...";
-        } else if (!$_POST['password']) {
-            $error = "password required...";
-        } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-            $error = "Not a valid email address...";
-        }
-
-        if ($_POST['loginActive'] == "0") {
-            console.log("login Active");
-        };
-
-        if ($error != "") {
-            echo $error;
-            exit()
-        }
+        print_r($_POST);
+        // $error = "";
+        //
+        // if (!$_POST['email']) {
+        //     $error = "email required...";
+        // } else if (!$_POST['password']) {
+        //     $error = "password required...";
+        // } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+        //     $error = "Not a valid email address...";
+        // }
+        //
+        // if ($_POST['loginActive'] == "0") {
+        //     console.log("login Active");
+        // };
+        //
+        // if ($error != "") {
+        //     echo $error;
+        //     exit()
+        // }
 
     }
 

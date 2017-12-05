04ed79b83afd865d7db24a9bd14257f7fd3593b9
permafrown <bjoelh@gmail.com>
2017-12-05 16:56:29 -0500

    adding permaTwit

--- twit/actions.php
+++ twit/actions.php
@@ -2,31 +2,29 @@
     // require_once("../permadmin/auth/config.php");
     include("functions.php");
 
-    // if ($_GET['action'] == 'loginSignup') {
-    //
-    //     $error = "";
-    //
-    //     if (!$_POST['email']) {
-    //         $error = "email required...";
-    //     } else if (!$_POST['password']) {
-    //         $error = "password required...";
-    //     } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
-    //         $error = "Not a valid email address...";
-    //     }
-    //
-    //     if ($_POST['loginActive'] == "0") {
-    //         console.log("login Active");
-    //     };
-    //
-    //     if ($error != "") {
-    //         echo $error;
-    //         exit()
-    //     }
-    //
-    // }
+    if ($_GET['action'] == 'loginSignup') {
 
-    if($_GET['action'] == 'loginSignup') {
-        print_r($POST);
+        $error = "";
+
+        if (!$_POST['email']) {
+            $error = "email required...";
+        } else if (!$_POST['password']) {
+            $error = "password required...";
+        } else if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+            $error = "Not a valid email address...";
+        }
+
+        if ($_POST['loginActive'] == "0") {
+            console.log("login Active");
+        };
+
+        if ($error != "") {
+            echo $error;
+            exit()
+        }
+
+    }
+
         // $error = '';
         //
         // $un = $_POST['email'];

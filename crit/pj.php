0adc09c18243908ccd02ef52a6047042fc1907a2
permafrown <bjoelh@gmail.com>
2017-12-14 11:06:44 -0500

    adding pj

---
+++ crit/pj.php
@@ -0,0 +1,26 @@
+<!-- Copyright 2017. GoodLife Music Ltd.. All Rights Reserved. -->
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+  <?php include_once '../includes/head.php';?>
+  <?php include_once '../permadmin/auth/config.php'; ?>
+  <link rel="stylesheet" href="css/pj.css">
+  <title>gerbil | permashutters</title>
+</head>
+<body>
+  <h1 class="title-centered">gerbil | permashutters</h1>
+  <hr class="permahr">
+  <div class="container-fluid">
+      <div class="col-xs-12">
+          <a href="index.php"><img class="img-responsive avatar center-block title-centered" src="http://bit.ly/permavatar" alt="avatar" /></a>
+      </div>
+  </div>
+  <hr class="permahr">
+
+  <script type="text/javascript" src="js/pj.js">
+
+  </script>
+  <?php include_once "includes/body_scripts.php"; ?>
+</body>
+</html>
